da52c5a8ad27e6ba0fe1c4e4639e34d61ec8c6b7
misha <amnezzia@gmail.com>
2018-12-25 19:43:18 -0800

    initial commit, copy from cranial-modeling

---
+++ README.rst
@@ -0,0 +1,10 @@
+
+
+Specialized objects and tools that act as python generators - does not load all data in memory, but can be iterated more than once from the beginning.
+
+Installation
+============
+
+.. code-block:: bash
+
+   pip install git+https://github.com/amnezzia/reiter
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='re_iter',
+      version='0.1',
+      description='Make python generators renewable',
+      long_description=long_description,
+      author='Misha, Tronc Data Team',
+      author_email='mikhail.erekhinsky@gmail.com',
+      url='https://github.com/amnezzia/reiter',
+      packages=find_packages(exclude=['tests*']),
+      install_requires=['pathos']
+      )
